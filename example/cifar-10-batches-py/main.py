softmax fixed for mini-batch; example improved

example/cifar-10-batches-py/main.py
@@ -29,42 +29,49 @@ for i in xrange(1, 6):
 
     train_labels += data['labels']
 
-train_images = train_images.reshape(-1, 3, 32, 32)
-train_images = train_images.astype(np.float32)
-train_images /= np.float32(255)
+# train_images = train_images[:100]
+# train_labels = train_labels[:100]
 
-r_mean = np.average(train_images[:, 0]).astype(np.float32)
-g_mean = np.average(train_images[:, 1]).astype(np.float32)
-b_mean = np.average(train_images[:, 2]).astype(np.float32)
+train_images = train_images.reshape(-1, 3, 32, 32)
+train_images = train_images.astype(np.float128)
+train_images /= 255.0
+
+r_mean = np.average(train_images[:, 0])
+g_mean = np.average(train_images[:, 1])
+b_mean = np.average(train_images[:, 2])
 
 train_images[:, 0] -= r_mean
 train_images[:, 1] -= g_mean
 train_images[:, 2] -= b_mean
 
+train_images = train_images.astype(np.float32)
+
 data = unpickle(sys.path[0] + '/test_batch')
 test_images = data['data'].reshape(-1, 3, 32, 32)
-test_images = test_images.astype(np.float32)
-test_images /= np.float32(255)
+test_images = test_images.astype(np.float128)
+test_images /= 255.0
 
 test_images[:, 0] -= r_mean
 test_images[:, 1] -= g_mean
 test_images[:, 2] -= b_mean
 
+test_images.astype(np.float32)
+
 test_labels = data['labels']
 
 lr = 1e-4
 dropout_percent = 0.5
-l2_reg = 4e-6
+l2_reg = 4e-7
 learning_rate_decay = np.float32(99e-2)
-batch_size = 1
+batch_size = 10
 
 cnn = NeuralNetwork(train_images.shape[1:],
                     [
-                        {'type': 'conv', 'k': 16, 'u_type': 'nag', 'f': 5, 's': 1, 'p': 2},
+                        {'type': 'conv', 'k': 16, 'u_type': 'adam', 'f': 5, 's': 1, 'p': 2},
                         {'type': 'pool'},
-                        {'type': 'conv', 'k': 20, 'u_type': 'nag', 'f': 5, 's': 1, 'p': 2},
+                        {'type': 'conv', 'k': 20, 'u_type': 'adam', 'f': 5, 's': 1, 'p': 2},
                         {'type': 'pool'},
-                        {'type': 'conv', 'k': 20, 'u_type': 'nag', 'f': 5, 's': 1, 'p': 2},
+                        {'type': 'conv', 'k': 20, 'u_type': 'adam', 'f': 5, 's': 1, 'p': 2},
                         {'type': 'pool'},
                         {'type': 'output', 'k': len(le.classes_), 'u_type': 'adam'}
                     ]
@@ -82,7 +89,7 @@ for i in xrange(60000000):
         print '{} epoch finish. learning rate is {}'.format(str(cnn.epoch_count), str(cnn.lr))
         cnn.lr *= learning_rate_decay
 
-        loss, acc = cnn.predict(train_images[-1000:], train_labels[-1000:])
+        loss, acc = cnn.predict(train_images[:4000], train_labels[:4000])
         print 'training acc:{}'.format(acc)
         print 'training loss:{}'.format(loss)
 
@@ -92,3 +99,5 @@ for i in xrange(60000000):
 
     cnn.t += 1
     loss, acc = cnn.epoch(train_images[start:end], train_labels[start:end])
+    # print loss
+    # print acc